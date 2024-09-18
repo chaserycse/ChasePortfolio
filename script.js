a56a67517de74d40f5aa31c94a375e9c466840b2
chaserycse <chaseall@buffalo.edu>
2024-09-18 19:17:15 -0400

    Updated Website
    Resume updated / dark mode removed

--- script.js
+++ script.js
@@ -4,8 +4,3 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
-
-function toggleDark(){
-    let element = document.body;
-    element.classList.toggle("dark-mode");
-}
